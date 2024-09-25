b91a50f6f734fce762253fbe5c976360ee8a5485
Tofriev <t.voelkel@posteo.de>
2024-09-25 15:48:31 +0200

    modified the hybrid functoin

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 import time
 from BruteForcing_Parallel_func import call_list_parallel
 from BruteForcing_Serial_func import call_list
+from BruteForcing_Hybrid_func import call_list_hybrid
 
 if __name__ == "__main__":
     pcb_list =  list(range(1, 6))
 
 
     start_time_parallel = time.time()
-    result_parallel = call_list_parallel(pcb_list)
+    result_parallel = call_list_hybrid(pcb_list)
     end_time_parallel = time.time()
     execution_time_parallel = end_time_parallel - start_time_parallel
 
